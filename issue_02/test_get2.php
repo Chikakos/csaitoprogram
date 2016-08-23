866ab91bde1604a50cdaff916c70ecd249e75c25
chikako saito <c-saito@sdl.co.jp>
2016-08-23 12:48:14 +0900

    issues#2_完成

--- issue_02/test_get2.php
+++ issue_02/test_get2.php
@@ -1,18 +1,27 @@
 <?php
-$comment = $_GET['comment'];
-echo $comment;
+  $comment = $_GET['comment'];
+  echo $comment;
 ?>
 <!DOCTYPE html>
 <html lang = "ja">
-<head>
-<meta charset = "UFT-8">
-<title>フォームからデータを受け取る</title>
-</head>
-<body>
-<h1>フォームデータの送信</h1>
-<form action = "index.php" method = "get">
-<input type = "text" name ="comment/"><br/>
-<input type = "submit" value ="送信">
-</form>
-</body>
+  <head>
+    <meta charset = "uft8">
+    <title>フォームからデータを受け取る</title>
+  </head>
+  <body>
+    <h1>フォームデータの送信</h1>
+    <!--
+    form
+      action: どこに送るか？=index.php→作る
+      method: get→データがURLで引き渡される
+            : post→データがURLで引き渡されない
+            *URLにデータを含めてもいい内容かどうか
+      input: name=入力内容に名前
+    -->
+
+    <form action = "test_get2.php" method = "get">
+        <input type = "text" name ="comment"><br/>
+        <input type = "submit" value ="送信">
+    </form>
+  </body>
 </html>
